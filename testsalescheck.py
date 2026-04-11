41f02a1577fdb812b7d41450659bc4bbd7db8e3d
jvgarcia14 <60375104+jvgarcia14@users.noreply.github.com>
2026-04-12 07:37:59 +0800

    Update testsalescheck.py

--- testsalescheck.py
+++ testsalescheck.py
@@ -93,6 +93,7 @@ TG_SAFE = 3900  # leave room for headers/markdown
 
 # ----------------- PAGES -----------------
 ALLOWED_PAGES = {
+    "#piper": "Piper",
     "#catelyn": "Catelyn",
     "#sethi": "Sethi",
     "#briBronDanAutumnOFTV": "BriBronDanAutumnOFTV",
